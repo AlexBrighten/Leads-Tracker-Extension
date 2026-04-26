9095966375fb69487444a8a395e41939c9cff314
alexbrighten <thealexjobbrighten@gmail.com>
2026-04-26 16:51:16 +0530

    feat: implement tab saving functionality, local storage persistence, and list rendering improvements

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const saveBtn = document.getElementById("save-btn")
 const inputEl = document.getElementById("input-el")
 const listEl = document.getElementById("list-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+let myLeads = []
 
-let myLeads = ["Alex", "Job", "Brighten"]
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
 
 saveBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    render()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
 })
 
-function render() {
+function render(leads) {
+
     let str = ""
-    for (let leads of myLeads) {
-        str += `<li>${leads}</li>`
+    for (let lead of leads) {
+        str += `<li><a target="_blank" href="${lead}">${lead}</a></li>`
     }
     listEl.innerHTML = str
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.removeItem("myLeads")
+    listEl.innerHTML = ""
+    myLeads = []
+})
